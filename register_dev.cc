59aa6692a666e5ca7b47dd149b33b4780b93dfaa
hongdayu <hongyu@yongding.com.cn>
2021-11-29 15:43:01 +0800

    将device 命名空间的命名改为 DEVICE

--- register_dev.cc
+++ register_dev.cc
@@ -3,12 +3,12 @@
 
 /*
    这一块儿就是驱动的业务层代码
-   第一步将你需要添加的设备声明到 device_n 的空间之内
+   第一步将你需要添加的设备声明到 DEVICE 的空间之内
    第二步完成的配置任务,根据json文本中提到的配置项
    第三步完成的完成你自己特定的注册流程;
 */
 
-namespace device_n{ //设备示例空间
+namespace DEVICE{ //设备示例空间
     zu6cg fpga;
 }
 
@@ -16,7 +16,7 @@ void config_fpga_device(json& fp,device* dev){
     std::string ver;
     fp.find("fpga","version",ver);
       if(!ver.find("zu6cg")){
-        device_n::fpga.config(fp);
+        DEVICE::fpga.config(fp);
         
     }
 }
@@ -28,5 +28,5 @@ void register_fpga(json& fp,platform* plat,device* dev)
 }
 
 void register_device(platform* plat,json& fp){
-    register_fpga(fp,plat,&device_n::fpga); //完整的FPGA设备注册流程 第一个设备流程
+    register_fpga(fp,plat,&DEVICE::fpga); //完整的FPGA设备注册流程 第一个设备流程
 }
